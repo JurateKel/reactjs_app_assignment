804c5a64aba15ae64c22b86cd410c66e4d812e76
JurateKel <jurate.kelmeliene@gmail.com>
2022-07-17 11:08:11 +0300

    Conversations ammount in toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -10,7 +10,12 @@ function Toolbar() {
     function logOut() {
         dispatch(loginUser())
     }
-
+    const messagesStore = useSelector(state => state.user.value.messages)
+    const userLogged = useSelector(state => state.user.value.userLoggedIn)
+    const userMessages = messagesStore.filter(x => x.participants.includes(userLogged.userName))
+    const chatFriends = []
+    userMessages.map(x=>chatFriends.push(x.participants.filter(y=> y !== userLogged.userName)))
+    const uniqueChatFriends = [ ...new Set(chatFriends.flat())]
 
   return (
     <div className='toolbar'>
@@ -20,7 +25,7 @@ function Toolbar() {
             <div>
                 <Link to="/profile">Profile</Link>
                 <Link to="/all_users">All users</Link>
-                <Link to="/conversations">Conversations</Link>
+                <Link to="/conversations">Conversations (total: {uniqueChatFriends.length})</Link>
             </div>
             <div>
                 <p>User: {userLoggedIn.userName}</p>
